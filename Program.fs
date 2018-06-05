7d274c2f539cc78ef7e12acd3a576058b19c9e2e
el <felicien.fotiomanfo@gmaill.com>
2018-06-05 15:46:09 +0100

    ...

--- Program.fs
+++ Program.fs
@@ -1,12 +1,27 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open Suave
-open Newtonsoft.Json
+open Suave.Operators
+open Suave.Filters
+open Suave.Successful
+
+
+
+let app = 
+    choose [ GET >=> choose
+                [ path "/" >=> OK "This is my Home Page"
+                  path "/hello" >=> OK "This is my GET Hello Page" ]
+             POST >=> choose
+                [ path "/hello" >=> OK "This is my POST Hello Page"
+                  path "/place-order" >=> OK "This is my place-order" ]
+    ]
+
+
 
 [<EntryPoint>]
 let main argv =
 
     
-    startWebServer defaultConfig (Successful.OK "Hello World!")
+    startWebServer defaultConfig app
 
     0 // return an integer exit code
