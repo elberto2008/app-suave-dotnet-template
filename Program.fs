ddf8b8f586365ec3f198fd9c10faf4f7573162e2
el <felicien.fotiomanfo@gmaill.com>
2018-06-05 01:09:42 +0100

    REAL SET UP IN PROGRESS...

---
+++ Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open Suave
+
+[<EntryPoint>]
+let main argv =
+
+    
+    startWebServer defaultConfig (Successful.OK "Hello World!")
+
+    0 // return an integer exit code
---
+++ test.fs
@@ -0,0 +1,6 @@
+namespace TEST
+
+open Suave
+
+module test = 
+    let b = 5
